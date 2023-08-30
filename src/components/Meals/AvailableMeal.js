b29ea507f9e62723c90f3d98029be93b86582b5e
PranavModi999 <pranavmodi999@gmail.com>
2023-08-30 14:42:02 -0400

    adding meals component

---
+++ src/components/Meals/AvailableMeal.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+const AvailableMeal = () => {
+  return <div>AvailableMeal</div>;
+};
+export default AvailableMeal;
---
+++ src/components/Meals/MealSummary.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import styles from "./MealSummary.module.css";
+
+const MealSummary = () => {
+  return (
+    <section className={styles.summary}>
+      <h2>Delicious Food, Delivered To You</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe amet
+        quam, earum sit cumque quasi libero possimus.
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt
+        reiciendis minima ducimus vitae omnis. Voluptates?
+      </p>
+    </section>
+  );
+};
+export default MealSummary;
